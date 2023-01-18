2fbaa8482d8eb8be86b47da80f89a3cd2b8a5406
yurimcf <yurimcf@gmail.com>
2023-01-18 20:03:56 -0300

    Otimização

--- src/br/com/americanas/polotech/view/menu/menuLivraria/MenuLivrariaAlterar.java
+++ src/br/com/americanas/polotech/view/menu/menuLivraria/MenuLivrariaAlterar.java
@@ -1,6 +1,7 @@
 package br.com.americanas.polotech.view.menu.menuLivraria;
 
 import br.com.americanas.polotech.model.DAO.EstoqueMetodos;
+import br.com.americanas.polotech.model.entity.Produto;
 
 import java.util.Scanner;
 
@@ -9,7 +10,7 @@ public class MenuLivrariaAlterar {
         Scanner src = new Scanner(System.in);
         Integer escolha;
         boolean validador = true;
-
+        Produto idPesq;
         do {
             System.out.println("================================");
             System.out.println("1. Alterar PREÇO do Produto");
@@ -24,7 +25,12 @@ public class MenuLivrariaAlterar {
                 case 1:
                     System.out.println("Forneça o ID do produto");
                     id = src.nextInt();
+                    idPesq = EstoqueMetodos.pesqItem(id);
+                    if (idPesq != null) {
+                        System.out.println(idPesq);
+                    }
                     System.out.println("Digite o novo Valor");
+                    src.nextLine();
                     Double valor = Double.parseDouble(src.nextLine());
 
                     EstoqueMetodos.alterarProduto(id, valor);
@@ -32,7 +38,11 @@ public class MenuLivrariaAlterar {
                 case 2:
                     System.out.println("Forneça o ID do produto");
                     id = src.nextInt();
-                    System.out.println("Digite a Quantidade certa");
+                    idPesq = EstoqueMetodos.pesqItem(id);
+                    if (idPesq != null) {
+                        System.out.println(idPesq);
+                    }
+                    System.out.println("Digite a nova quantidade");
                     Integer qtd = src.nextInt();
 
                     EstoqueMetodos.alterarProduto(id, qtd);
@@ -41,7 +51,7 @@ public class MenuLivrariaAlterar {
                     validador = false;
                     break;
                 default:
-                    System.out.println("Digite uma Opção Válida");
+                    System.out.println("Digite uma Opção Válida 1 a 3");
                     break;
             }
         }while (validador);
